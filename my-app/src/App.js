49c120450dbddb51c735aaa419651f2ee200a3fb
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-13 13:08:32 -0400

    Added Page Links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
+//Components
+import Navigation from './Components/Navbar';
+import Home from './Components/Pages/Home';
+import Work from './Components/Pages/Work';
+import Contact from './Components/Pages/Contact';
 
 function App() {
   return (
     <div className="App">
-   hi
+      <Navigation />
+  <Routes>
+<Route element={<Home/>} path={'/'} />
+<Route element={<Work/>} path={'/work'} />
+<Route element={<Contact />} path={'/contact'} />
+  </Routes>
     </div>
   );
 }

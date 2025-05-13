6e9738a9d09521daffc1429164cf4af443ac3dd6
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-13 14:41:41 -0400

    Designed Homepage

--- my-app/src/Components/Pages/Home.js
+++ my-app/src/Components/Pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../../Styles/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-page'>
+<h1>Mikaila Ivery</h1>
+<div className='home-bio' >Graphic Design, Code, Art. </div>
+<ul>
+    <li><a className='home-link' href='/work'>Work</a></li>
+    <li><a className='home-link' href='/#about'>About</a></li>
+    <li><a className='home-link' href='/contact'>Contact</a></li>
+</ul>
+    </div>
   )
 }
 

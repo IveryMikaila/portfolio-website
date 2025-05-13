89e50f667fa33888df5abfef1ed537e1b0b69d71
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-13 13:33:24 -0400

    Added logic for nav menu

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
+import { Squash as Hamburger } from 'hamburger-react'
+
 
 const Navbar = () => {
+ const [isOpen, setOpen] = useState(false)
+
+ function NavOpen (){
+return(<>
+<div>
+ <a href='/'>Home</a>
+ <a href='/work'>Work</a>
+ <a href='/contact'>Contact</a>
+</div>
+</>)
+
+ }
+
   return (
-    <div>Navbar</div>
+    <div>
+
+
+<Hamburger toggled={isOpen} toggle={setOpen} className='nav-hamburger' />
+{isOpen ? <NavOpen /> : <div style={{display:"none"}}></div> }
+
+    </div>
   )
 }
 

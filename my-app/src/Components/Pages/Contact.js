cdbf8ca953f735bf307437c5af235be889af659c
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-13 15:52:37 -0400

    Added the Form logic

--- my-app/src/Components/Pages/Contact.js
+++ my-app/src/Components/Pages/Contact.js
@@ -1,8 +1,66 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "31169d91-4124-4598-8266-3fec075864fa");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
   return (
-    <div>Contact</div>
+    <div>
+        <h1>Let's Chat!</h1>
+        <div className='contact-page'>
+        <div className='form'>
+        <form onSubmit={onSubmit}>
+        <input type="text" name="name" required/>
+        <input type="email" name="email" required/>
+        <textarea name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+      </form>
+      <span>{result}</span>
+</div>
+<div className='contact-side'>
+<h3>Contact Information </h3>
+<p>
+<ul>
+  <li><span className='bold'>Email:</span> iverymikaila@gmail.com</li>
+  <li><span className='bold'>Phone:</span> Request via Email</li>
+</ul>
+</p>
+<h3>Social Media </h3>
+<ul>
+    <li><a href='https://www.instagram.com/fkamynk/' target='_blank'>Instagram</a></li>
+    <li><a href='https://www.pinterest.com/fkamynk/_pins/' target='_blank'>Pinterest</a></li>
+    <li><a href='https://www.youtube.com/@Fkamynk' target='_blank'>YouTube</a></li>
+</ul>
+</div>
+        </div>
+      
+    </div>
   )
 }
 

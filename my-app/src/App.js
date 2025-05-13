65166c85139e7de4738f64cefb745c75f6aab295
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-13 14:09:57 -0400

    Made nav menu responsive

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import './Styles/Index.css'
 
 //Components
 import Navigation from './Components/Navbar';

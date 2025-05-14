260599d587d5c300ef7545f9cafdbfe9131375ec
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2025-05-14 14:48:13 -0400

    Created an internal database for portfolio information

---
+++ my-app/src/Components/Data.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {webDesignAsset} from '../assets/webdesign_WaistAwaybyMoojy.mov'
+
+export let data = {
+
+    Bio:"I'm a creative and detail-oriented Graphic Designer with a strong foundation in visual storytelling and technical proficiency in front-end coding. A proud graduate of the Fashion Institute of Technology (FIT), blending artistic vision with functional design to create engaging digital and print experiences. Skilled in Adobe Creative Suite, HTML, CSS, and JavaScript, with a passion for designing intuitive user interfaces and seamless user experiences.",
+    Skills:['Adobe InDesign', 'Adobe Illustrator','Adobe Photoshop','HTML','CSS',
+'JavaScript', 'React.js', 'Editorial Layout Design', 'Typography & Typesetting','Print & Digital Publication Design','Color Theory & Composition','Art Direction','Attention to Detail', 'Time Management','Creative Problem Solving'],
+Personal: "In my free time, I enjoy reading, playing video games, and of course, creating art. I specialize in realistic portraiture mainly in two mediums, oil paint and graphite.",
+webDesignAssets:`${webDesignAsset}`, 
+graphicDesignAssets:[],
+
+}
